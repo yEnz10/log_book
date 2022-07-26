624c0d44abb93a8c73bab038da0e43443af3e5c7
yarmyen <phongsathon.yen@gmail.com>
2022-07-26 08:46:25 +0700

    [fix] : log book

--- models/log_book.py
+++ models/log_book.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
-
 class LogBook(models.Model):
-    _name = 'log_book.profile'
+    _name = "log.book"
+    _description = "Log Book"
 
-    name = fields.Char(string ="Name")
-    email = fields.Char(string = "Email")
-    phone = fields.Char(string ="Phone")
-#     _description = 'c:\users\phong\documents\101\odoo\mymodules\log_book.c:\users\phong\documents\101\odoo\mymodules\log_book'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    # required=True, translate=True
+    name = fields.Char(string='Name', required=True)
+    destination = fields.Char(string='Destination')
