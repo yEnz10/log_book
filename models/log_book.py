88c06e467b590728d86d0ce9ee1ed84869836615
yarmyen <phongsathon.yen@gmail.com>
2022-07-31 23:36:30 +0700

    commit

--- models/log_book.py
+++ models/log_book.py
@@ -2,9 +2,23 @@
 from odoo import models, fields, api
 
 class LogBook(models.Model):
-    _name = "log.book"
-    _description = "Log Book"
+    _name = 'log.book'
+    _description = 'Log Book'
 
     # required=True, translate=True
-    name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(string='No.')
+    recipient_name = fields.Char(string='Recipient name', required=True)
     destination = fields.Char(string='Destination')
+    barcode_number = fields.Char(string='Barcode number (9 Digits)', default=9)
+    weight = fields.Char(string='Weight')
+    baht = fields.Char(string='Baht')
+    satang = fields.Char(string='Satang')
+    note = fields.Text(string='Note')
+    
+class LogBookHIV(models.Model):
+    _name = 'log.book.hiv'
+    _description = 'Log Book HIV'
+
+    # required=True, translate=True
+    unit_number = fields.Char(string='Unit number', required=True)
+    
