5df302e2cff6ef643efb53a4cf9421e28f3dedbf
yen <Phongsathon.w@gmail.com>
2022-08-01 16:08:25 +0700

    commit <3

--- models/log_book.py
+++ models/log_book.py
@@ -4,16 +4,24 @@ from odoo import models, fields, api
 class LogBook(models.Model):
     _name = 'log.book'
     _description = 'Log Book'
+    _order = "sequence,id"
 
     # required=True, translate=True
-    sequence = fields.Integer(string='No.')
+    sequence = fields.Integer(string='No.', readonly=1)
+    # log_book_seq = fields.Integer(string='No.', readonly=1)
     recipient_name = fields.Char(string='Recipient name', required=True)
     destination = fields.Char(string='Destination')
-    barcode_number = fields.Char(string='Barcode number (9 Digits)', default=9)
+    barcode_number = fields.Char(string='Barcode number (9 Digits)')
     weight = fields.Char(string='Weight')
     baht = fields.Char(string='Baht')
     satang = fields.Char(string='Satang')
     note = fields.Text(string='Note')
+
+    # @api.model
+    # def create(self, vals):
+    #     print("Log Book create vals ",vals)
+    #     vals['log_book_seq'] = self.env['ir.sequence'].next_by_code('log.book')
+    #     return super(LogBook, self).create(vals)
     
 class LogBookHIV(models.Model):
     _name = 'log.book.hiv'
